40f37976c3d520446812e35ae56c8ca5da06d0b8
Bahar Aydemir <bahar.aydemir@triosstudent.com>
2026-02-22 14:08:58 -0500

    Update JS and CSS files

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 // Get elements
 const firstNameInput = document.getElementById('firstName');
-const lastNameInput = document.getElementById('lastName');
+const lastNameInput = document.getElementById('lastName');function changeText() {
+    document.getElementById("title").innerText = "You clicked the button!";
+}
+
+function changeText() {
+    document.getElementById("title").innerText = "You clicked the button!";
+}
